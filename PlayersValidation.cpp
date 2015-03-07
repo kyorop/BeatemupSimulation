5bcec0396cc75733a88be2f6d479b88907ea1b5a
しおから <range_8591@yahoo.co.jp>
2015-03-07 18:55:09 +0900

    動作確認中に表示される文字列を変更

--- PlayersValidation.cpp
+++ PlayersValidation.cpp
@@ -3,11 +3,8 @@
 
 void PlayersValidation::Validation(PLAYER *player)
 {
-	/*double* playerY = player->GetPosY();*/
 	DrawString(0, 16, "PLAYERSVALIDATION", GetColor(0, 0, 0));
 	DrawString(0, 0, "Wキーを押すと動作確認を終了", GetColor(0, 0, 0));
-	/*DrawFormatString(0, 16, GetColor(0, 0, 0), "Y座標：%lf", &playerY);
-*/
 	if (CheckHitKey(KEY_INPUT_W)){ ValidationEnd(); }
 	player->PlayerMove();
 	player->GraphNumChange();
