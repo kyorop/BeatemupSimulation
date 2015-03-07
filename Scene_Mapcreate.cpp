d4b694ccd196b3aab2d100b7db5ac0a2cdf5cbc8
Kyoro_p <longcorridor@outlook.jp>
2015-03-07 18:48:45 +0900

    オブジェクトを表示するウィンドウを作成

--- Scene_Mapcreate.cpp
+++ Scene_Mapcreate.cpp
@@ -3,6 +3,19 @@
 
 void Scene_Mapcreate::UpdateScene()
 {
+
+}
+
+Scene_Mapcreate::Scene_Mapcreate()
+	:objWindowX1(10),
+	objWindowY1(360 + 10),
+	objWindowX2(objWindowX1 + 640 - 20),
+	objWindowY2(objWindowY1 + 100),
+	groundX1(0),
+	groundY1(280),
+	groundX2(640),
+	groundY2(groundY1)
+{
 }
 
 void Scene_Mapcreate::Update()
@@ -12,4 +25,6 @@ void Scene_Mapcreate::Update()
 void Scene_Mapcreate::Draw()
 {
 	DrawFormatString(0, 0, GetColor(255, 255, 255), "This scene is Mapcreate");
+	DrawLine(groundX1, groundY1, groundX2, groundY2, GetColor(255, 0, 0), 4);
+	DrawBox(objWindowX1, objWindowY1, objWindowX2, objWindowY2, GetColor(255, 0, 0), false);
 }
--- Scene_Mapcreate.h
+++ Scene_Mapcreate.h
@@ -4,8 +4,19 @@
 class Scene_Mapcreate
 	:public Scene_Base
 {
+	const int objWindowX1;
+	const int objWindowY1;
+	const int objWindowX2;
+	const int objWindowY2;
+
+	const int groundX1;
+	const int groundY1;
+	const int groundX2;
+	const int groundY2;
+
 	void UpdateScene() override;
 public:
+	Scene_Mapcreate();
 	void Update() override;
 	void Draw() override;
 public:
