0f2158a0d51a3cae24b995ce23b447544f89dda1
Kyoro_p <longcorridor@outlook.jp>
2015-03-08 05:09:39 +0900

    既にあるオブジェクトより下にドロップした時は戻るようにした。

--- Object.cpp
+++ Object.cpp
@@ -4,6 +4,7 @@
 Object::Object()
 {
 	m_click_flag = FALSE;
+	initialized = false;
 }
 
 
@@ -14,6 +15,15 @@ Object::~Object()
 
 void Object::SetObject(int posx, int posy, int sizex, int sizey)
 {
+	if (!initialized)
+	{
+		initDrawX1 = posx;
+		initDrawY1 = posy;
+		initDrawWidth = sizex;
+		initDrawHeight = sizey;
+
+		initialized = true;
+	}
 	m_draw_pos_x = m_pos_x = posx;
 	m_draw_pos_y = m_pos_y = posy;
 	m_draw_size_x = m_size_x = sizex;
@@ -43,4 +53,15 @@ bool Object::CheckHitMouse(int mouse_x, int mouse_y)
 		return TRUE;
 	}
 	return FALSE;
+}
+
+void Object::ResetDrawPos()
+{
+	if (initialized)
+	{
+		m_draw_pos_x = initDrawX1;
+		m_draw_pos_y = initDrawY1;
+		m_draw_size_x = m_size_x = initDrawWidth;
+		m_draw_size_y = m_size_y = initDrawHeight;
+	}
 }
